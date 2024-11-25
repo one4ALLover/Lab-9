37efa718a20bbd9df51779a5f4e6afad38789d92
one4ALLover <egorrvanijked@gmail.com>
2024-11-25 22:21:22 +0300

    task_1.3_done

--- main.cpp
+++ main.cpp
@@ -1,85 +1,83 @@
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <iostream>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 #include <queue>
-#include <cstdlib>
-#include <ctime>
-#include <Windows.h>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
-vector<vector<int>> generateAdjacencyM(int n) {
-    vector<vector<int>> matrix(n, vector<int>(n, 0));
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            if (rand() % 2 == 1) {
-                matrix[i][j] = 1;
-                matrix[j][i] = 1;
+void createAdjList(vector<vector<int>>& adjList, int N) {
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            int edge = rand() % 2;
+            if (edge == 1) {
+                adjList[i].push_back(j);
+                adjList[j].push_back(i);
             }
         }
     }
-    return matrix;
 }
 
-vector<int> bfsDistance(const vector<vector<int>>& G, int start) {
-    int n = G.size();
-    vector<int> DIST(n, -1);
-    queue<int> Q;
+void printAdjList(const vector<vector<int>>& adjList, int N) {
+    printf("Список смежности:\n");
+    for (int i = 0; i < N; i++) {
+        printf("Вершина %d: ", i+1);
+        for (int j : adjList[i]) {
+            printf("%d ", j+1);
+        }
+        printf("\n");
+    }
+}
 
-    Q.push(start);
-    DIST[start] = 0;
+
+void BFS(const vector<vector<int>>& adjList, int N, int v, vector<int>& DIST) {
+
+    DIST.assign(N, -1);
+
+    queue<int> Q;
+    Q.push(v);
+    DIST[v] = 0;
 
     while (!Q.empty()) {
         int v = Q.front();
         Q.pop();
-        cout << v+1 << " ";
 
-        for (int i = 0; i < n; ++i) {
-            if (G[v][i] == 1 && DIST[i] == -1) {
-                Q.push(i);
-                DIST[i] = DIST[v] + 1;
+        for (int neighbor : adjList[v]) {
+            if (DIST[neighbor] == -1) {
+                Q.push(neighbor);
+                DIST[neighbor] = DIST[v] + 1;
             }
         }
-    }
-
-    return DIST;
-}
-
-void printMatrix(const vector<vector<int>>& matrix) {
-    cout << "Матрица смежности:" << endl;
-    for (const auto& row : matrix) {
-        for (int val : row) {
-            cout << val << " ";
-        }
-        cout << endl;
     }
 }
 
 int main() {
-
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    srand(time(0));
+    srand(2364892365489);
 
-    int n;
-    cout << "Введите количество вершин в графе: ";
-    cin >> n;
+    int N;
+    printf("Введите количество вершин в графе: ");
+    scanf("%d", &N);
 
-    vector<vector<int>> adjacencyM = generateAdjacencyM(n);
-    printMatrix(adjacencyM);
+    vector<vector<int>> adjList(N);
+    createAdjList(adjList, N);
+    printAdjList(adjList, N);
 
     int startVertex;
-    cout << "Введите начальную вершину (1 - " << n << "): ";
-    cin >> startVertex;
+    printf("Введите исходную вершину для поиска расстояний (1 - %d): ", N);
+    scanf("%d", &startVertex);
+    startVertex = startVertex - 1;
 
-    cout << "Посещенные вершины: " << endl;
-    vector<int> distances = bfsDistance(adjacencyM, startVertex-1);
+    vector<int> DIST;
+    BFS(adjList, N, startVertex, DIST);
 
     cout << endl << "Расстояния от вершины " << startVertex << ":" << endl;
-    for (int i = 0; i < n; ++i) {
-        cout << "До вершины " << i+1 << " расстояние: " << distances[i] << endl;
+    for (int i = 0; i < N; ++i) {
+            cout << "До вершины " << i+1 << " расстояние: " << DIST[i] << endl;
     }
 }
