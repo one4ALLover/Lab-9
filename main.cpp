391266ef4fa3cbd5b4a17514c22732f3cd897b47
one4ALLover <egorrvanijked@gmail.com>
2024-11-25 22:27:18 +0300

    task_2.2_done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -8,48 +6,44 @@
 
 using namespace std;
 
-void createG(vector<vector<int>>& G, int N) {
+void createGraph(vector<vector<int>>& adjList, int N) {
     for (int i = 0; i < N; i++) {
         for (int j = i + 1; j < N; j++) {
             int edge = rand() % 2;
-            G[i][j] = edge;
-            G[j][i] = edge;
+            if (edge) {
+                adjList[i].push_back(j);
+                adjList[j].push_back(i);
+            }
         }
-    }
-
-    for (int i = 0; i < N; i++) {
-        G[i][i] = 0;
     }
 }
 
-void printG(const vector<vector<int>>& G) {
-    cout << "Матрица смежности:" << endl;
-    for (const auto& row : G) {
-        for (int val : row) {
-            cout << val << " ";
+void printGraph(const vector<vector<int>>& adjList) {
+    for (int i = 0; i < adjList.size(); i++) {
+        cout << i + 1 << ": ";
+        for (int next : adjList[i]) {
+            cout << next + 1 << " ";
         }
         cout << endl;
     }
 }
 
-void DFS(const vector<vector<int>>& G, vector<int>& DIST, int v, int start, vector<bool>& visited) {
-    if (DIST[v] == -1 || start < DIST[v]) {
-        DIST[v] = start;
+
+void DFS(const vector<vector<int>>& adjList, vector<int>& DIST, int v, int currentDist, vector<bool>& visited) {
+    if (DIST[v] == -1 || currentDist < DIST[v]) {
+        DIST[v] = currentDist;
     }
 
     visited[v] = true;
 
-    cout << v + 1 << " ";
-
-    for (int i = 0; i < G.size(); i++) {
-        if (G[v][i] == 1 && !visited[i]) {
-            DFS(G, DIST, i, start + 1, visited);
+    for (int next : adjList[v]) {
+        if (!visited[next]) {
+            DFS(adjList, DIST, next, currentDist + 1, visited);
         }
     }
 
     visited[v] = false;
 }
-
 
 int main() {
     SetConsoleCP(1251);
@@ -61,18 +55,18 @@ int main() {
     cout << "Введите количество вершин в графе: ";
     cin >> N;
 
-    vector<vector<int>> G(N, vector<int>(N, 0));
-    createG(G, N);
-    printG(G);
+    vector<vector<int>> adjList(N);
+    createGraph(adjList, N);
+    printGraph(adjList);
 
     int startVertex;
     cout << "Введите начальную вершину (1 - " << N << "): ";
     cin >> startVertex;
 
     vector<int> DIST(N, -1);
-    cout << "Посещенные вершины: " << endl;
-    vector<bool> visited(G.size(), false);
-    DFS(G, DIST, startVertex - 1, 0, visited);
+    vector<bool> visited(N, false);
+
+    DFS(adjList, DIST, startVertex - 1, 0, visited);
 
     cout << endl << "Расстояния от вершины " << startVertex << ":" << endl;
     for (int i = 0; i < N; i++) {
